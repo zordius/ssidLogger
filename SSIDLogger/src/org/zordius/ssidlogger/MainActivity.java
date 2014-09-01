bfd8fcae811851938862ab5b914bee7046d19b61
Zordius Chen <zordius@yahoo-inc.com>
2014-09-01 10:45:16 +0800

    refine layout, add some strings

--- SSIDLogger/src/org/zordius/ssidlogger/MainActivity.java
+++ SSIDLogger/src/org/zordius/ssidlogger/MainActivity.java
@@ -2,8 +2,6 @@ package org.zordius.ssidlogger;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
