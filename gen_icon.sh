3f67502d3a84b3570003d211b98916f0cb52b2f1
Zordius Chen <zordius@yahoo-inc.com>
2014-09-01 13:12:06 +0800

    using the svg logo

---
+++ gen_icon.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+test -d ~/node_modules/svg2png || npm install svg2png
+node -e "require('svg2png')('logo.svg', 'logo.png', function(err) {err ? console.warn(err) : console.log('Done!');});"
+node -e "require('svg2png')('logo.svg', 'SSIDLogger/res/drawable-xxhdpi/ic_launcher.png', 0.45, function(err) {err ? console.warn(err) : console.log('144x144 Done!');});"
+node -e "require('svg2png')('logo.svg', 'SSIDLogger/res/drawable-xhdpi/ic_launcher.png', 0.3, function(err) {err ? console.warn(err) : console.log('96x96 Done!');});"
+node -e "require('svg2png')('logo.svg', 'SSIDLogger/res/drawable-hdpi/ic_launcher.png', 0.225, function(err) {err ? console.warn(err) : console.log('72x72 Done!');});"
+node -e "require('svg2png')('logo.svg', 'SSIDLogger/res/drawable-mdpi/ic_launcher.png', 0.15, function(err) {err ? console.warn(err) : console.log('48x48 Done!');});"
