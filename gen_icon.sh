e5c6d38e5539ea5f6605243d72a4254794371ae5
Zordius Chen <zordius@yahoo-inc.com>
2015-08-12 17:44:06 +0800

    refine gen_icon.sh

--- gen_icon.sh
+++ gen_icon.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 test -d ~/node_modules/svg2png || npm install svg2png
-node -e "require('svg2png')('logo.svg', 'SSIDLogger/ic_launcher-web.png', 1.6, function(err) {err ? console.warn(err) : console.log('512x512 Done!');});"
-node -e "require('svg2png')('logo.svg', 'SSIDLogger/res/drawable-xxhdpi/ic_launcher.png', 0.45, function(err) {err ? console.warn(err) : console.log('144x144 Done!');});"
-node -e "require('svg2png')('logo.svg', 'SSIDLogger/res/drawable-xhdpi/ic_launcher.png', 0.3, function(err) {err ? console.warn(err) : console.log('96x96 Done!');});"
-node -e "require('svg2png')('logo.svg', 'SSIDLogger/res/drawable-hdpi/ic_launcher.png', 0.225, function(err) {err ? console.warn(err) : console.log('72x72 Done!');});"
-node -e "require('svg2png')('logo.svg', 'SSIDLogger/res/drawable-mdpi/ic_launcher.png', 0.15, function(err) {err ? console.warn(err) : console.log('48x48 Done!');});"
+node -e "require('svg2png')('logo.svg', 'ic_launcher-web.png', 1.6, function(err) {err ? console.warn(err) : console.log('512x512 Done!');});"
+node -e "require('svg2png')('logo.svg', 'SSIDLogger/app/src/main/res/drawable-xxhdpi/ic_launcher.png', 0.45, function(err) {err ? console.warn(err) : console.log('144x144 Done!');});"
+node -e "require('svg2png')('logo.svg', 'SSIDLogger/app/src/main/res/drawable-xhdpi/ic_launcher.png', 0.3, function(err) {err ? console.warn(err) : console.log('96x96 Done!');});"
+node -e "require('svg2png')('logo.svg', 'SSIDLogger/app/src/main/res/drawable-hdpi/ic_launcher.png', 0.225, function(err) {err ? console.warn(err) : console.log('72x72 Done!');});"
+node -e "require('svg2png')('logo.svg', 'SSIDLogger/app/src/main/res/drawable-mdpi/ic_launcher.png', 0.15, function(err) {err ? console.warn(err) : console.log('48x48 Done!');});"
